e6a576531e9ed5f595731945e940e4d38617748f
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-03-28 19:16:24 +0530

    Update factorial.java

--- OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/Code Excerise 3/factorial.java
+++ OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/Code Excerise 3/factorial.java
@@ -1,16 +1,12 @@
 package EasyLevelAssignment;
-
-class factorial {
-        static int factorial(int n)
-        {
-            if (n == 0)
-                return 1;
-
-            return n*factorial(n-1);
-        }
-        public static void main(String[] args)
-        {
-            int num = 10;
-            System.out.println("Factorial of "+ num + " is " + factorial(num));
-        }
+public class FactorialNum {
+    public static int factorial(int Number){
+        if (Number == 0)
+            return 1;
+        return Number*factorial(Number-1);
     }
+    public static void main(String[] args){
+        int Fnum = 10;
+        System.out.println("Factorial of "+ Fnum + " is " + factorial(Fnum));
+    }
+}
