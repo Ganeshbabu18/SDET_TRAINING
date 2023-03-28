ee504ef782f7f3f263c916f932f846bcac533b58
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-03-28 19:46:19 +0530

    Update OddEven.java

--- OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/Code Excerise 1/OddEven.java
+++ OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/Code Excerise 1/OddEven.java
@@ -1,6 +1,6 @@
 package EasyLevelAssignment;
 public class OddEven {
-        public void Even_Odd_Number(int number) {
+        public void Odd_Even_Number(int number) {
             //  int num = 11;
             if (number % 2 == 0) {
                 System.out.println("The given Number is even");
@@ -9,7 +9,7 @@ public class OddEven {
             }
         }
         public static void main(String[] args) {
-            OddEven EvenOdd = new OddEven();
-            EvenOdd.Even_Odd_Number(11);
+            OddEven Number = new OddEven();
+            Number.Odd_Even_Number(11);
         }
     }
