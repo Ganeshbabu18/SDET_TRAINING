346d31ebafd786a736d3af44a5c74d49ac432624
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-03-28 18:12:52 +0530

    Update Midlevel3.java

--- OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/MidLevel3/Midlevel3.java
+++ OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/MidLevel3/Midlevel3.java
@@ -1,23 +1,18 @@
-package MidLevel_Assignment.Midlevel1;
+package MidLevelAssignment;
 import java.util.Arrays;
-    public class Midlevel3 {
+    public class MidlevelThree {
         public static void swapEnds() {
             int[] num = {1, 2, 3, 7};
             int numSize = num.length;
-            String b = Arrays.toString(num);
-            System.out.println(b);
+            String Swaping = Arrays.toString(num);
+            System.out.println(Swaping);
             int lastNum = num[numSize - 1];
-            if (lastNum % 2 != 0) {
-                int first = num[0];
-                num[0] = num[numSize - 1];
-                num[numSize - 1] = first;
-                String b1 = Arrays.toString(num);
-                System.out.println("The last number of array is a prime number " + lastNum);
-                System.out.println(b1);
-            } else {
-                System.out.println("The last number of array is not prime number " + lastNum);
-                System.out.println(b);
-            }
+            int first = num[0];
+            num[0] = num[numSize - 1];
+            num[numSize - 1] = first;
+            String sObject = Arrays.toString(num);
+            System.out.println("The last number of array is a prime number " + lastNum);
+            System.out.println(sObject);
         }
         public static void main(String[] args) {
             swapEnds();
