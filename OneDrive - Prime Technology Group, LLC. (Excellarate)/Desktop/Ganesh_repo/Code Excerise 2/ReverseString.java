c1c68c7b05e88b04182fa358fabd365ff7c86ba1
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-03-28 19:21:49 +0530

    Update ReverseString.java

--- OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/Code Excerise 2/ReverseString.java
+++ OneDrive - Prime Technology Group, LLC. (Excellarate)/Desktop/Ganesh_repo/Code Excerise 2/ReverseString.java
@@ -2,18 +2,15 @@ package EasyLevelAssignment;
 import java.io.*;
 import java.util.Scanner;
 public class ReverseString {
-
-        public static void main (String[] args) {
-
-            String str= "Ganesh", nstr="";
-            char word;
-            System.out.println("Original word is Ganesh");
-
-            for (int i=0; i<str.length(); i++)
-            {
-                word = str.charAt(i);
-                nstr= word + nstr;
-            }
-            System.out.println("Reversed word is "+ nstr);
+    public static void main (String[] args) {
+        String str= "Ganesh Babu", nstr="";
+        char word;
+        System.out.println("Original word is Ganesh Babu");
+        for (int Reverse=0; Reverse<str.length(); Reverse++)
+        {
+            word = str.charAt(Reverse);
+            nstr= word + nstr;
         }
+        System.out.println("Reversed word is "+ nstr);
     }
+}
